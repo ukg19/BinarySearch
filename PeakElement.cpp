2067c72fd2ad030ab8595939a4b854e4bde44f39
Utkarsh Katiyar <utkarshkatiyar19@gmail.com>
2020-03-16 22:53:13 +0530

    Create PeakElement.cpp

---
+++ PeakElement.cpp
@@ -0,0 +1,42 @@
+/*
+Link :  https://leetcode.com/problems/find-peak-element/
+A peak element is an element that is greater than its neighbors.
+
+Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 1 or 5 
+Explanation: Your function can return either index number 1 where the peak element is 2, 
+             or index number 5 where the peak element is 6.
+Note:
+
+Your solution should be in logarithmic complexity.
+*/
+class Solution {
+public:
+    int b_search(vector<int>& nums, int l, int r){
+        if(l == r)
+            return l;
+        int mid = (l+r)/2;
+        if(nums[mid] < nums[mid+1])
+            return b_search(nums, mid+1, r);
+        
+        return b_search(nums, l, mid);
+        
+    }
+    int findPeakElement(vector<int>& nums) {
+        int sz = nums.size();
+        return b_search(nums, 0, sz-1);
+    }
+};
